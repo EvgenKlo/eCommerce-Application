2443587f7f8fd6678d5b0bd7052e13879618699b
fasty86 <oleg.tolm1986@gmail.com>
2023-08-06 09:09:56 +0300

    feat: add polyfils plugins to vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { configDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import autoprefixer from 'autoprefixer';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export const pathResolver = (p: string) => resolve(__dirname, '.', p);
 
@@ -16,6 +19,26 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': pathResolver('./src'),
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
     },
   },
   test: {
