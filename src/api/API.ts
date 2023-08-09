f4b36b35f5d11948e7aa1b0539e9838bb6fb2ae2
fasty86 <oleg.tolm1986@gmail.com>
2023-08-09 16:58:58 +0300

    feat: add class for creating new customer

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,5 +1,10 @@
-import { Project, Customer } from '@commercetools/platform-sdk/dist/declarations/src/generated';
+import {
+  Project,
+  Customer,
+  CustomerSignInResult,
+} from '@commercetools/platform-sdk/dist/declarations/src/generated';
 import { apiRoot } from './lib/Client';
+import { createCustomer } from '@/store/slices/customerSlice';
 
 export class API {
   static async getProject() {
@@ -21,6 +26,17 @@ export class API {
         .withId({ ID: '057f1155-2b8a-4295-bd94-1ca97fe10b30' })
         .get()
         .execute();
+      result = body;
+    } catch (error) {
+      console.log(error);
+    }
+    return result;
+  }
+
+  static async createCustomer(customer: createCustomer) {
+    let result = {} as CustomerSignInResult;
+    try {
+      const { body } = await apiRoot.customers().post({ body: customer }).execute();
       result = body;
     } catch (error) {
       console.log(error);
