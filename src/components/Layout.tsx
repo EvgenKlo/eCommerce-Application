103e0aa6b24d4a95ffaf9b0f9ed83b68b276ff36
lidasharova <Lida050496@yandex.ru>
2023-08-10 01:27:48 +0300

    feat: add header & footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
-import Header from './UI/Header';
 import { ReactElement } from 'react';
+import { Header } from './UI/Header';
+import { Footer } from './UI/Footer';
 
 function Layout(): ReactElement {
   return (
     <>
       <Header />
-      <Outlet />
-      <footer>2023 RS team task</footer>
+      <div className="content">
+        <Outlet />
+      </div>
+      <Footer />
     </>
   );
 }
--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,107 +1,220 @@
-import { Box, Typography, Toolbar, AppBar, Link, IconButton, Button } from '@mui/material';
-import { ReactElement } from 'react';
+import React from 'react';
+import {
+  AppBar,
+  Badge,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  Tooltip,
+  MenuItem,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link as RouterLink } from 'react-router-dom';
-import { ShoppingBasketOutlined } from '@mui/icons-material';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PetsIcon from '@mui/icons-material/Pets';
 
-function Header(): ReactElement {
+const styleSighLinks = {
+  color: '#ffffff',
+  fontSize: '0.9rem',
+  marginX: 1,
+  '&:hover': {
+    color: '#FF8C00',
+  },
+};
+
+const pages = ['Catalog', 'About', 'Main', 'Profile'];
+
+const signLinks = [
+  { text: 'Sign in', link: '/login' },
+  { text: 'Sign up', link: '/registration' },
+];
+
+export function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <AppBar position="static">
-      <Toolbar
-        variant="dense"
-        sx={{ display: 'flex', justifyContent: 'space-between' }}
-      >
-        <Box sx={{ display: { xs: 'flex', sm: 'flex' }, gap: 2 }}>
-          <Typography
-            color="#fff"
-            variant="h5"
-            component="div"
-            sx={{ width: 100 }}
-          >
-            <Link
-              component={RouterLink}
-              to="/"
-              color="inherit"
-              underline="hover"
-            >
-              main
-            </Link>
-          </Typography>
-          <Typography
-            color="#fff"
-            variant="h5"
-            component="div"
-            sx={{ width: 100 }}
-          >
-            <Link
-              component={RouterLink}
-              to="/login"
-              color="inherit"
-              underline="hover"
-            >
-              login
-            </Link>
-          </Typography>
-          <Typography
-            color="#fff"
-            variant="h5"
-            component="div"
-            sx={{ width: 100 }}
-          >
-            <Link
-              component={RouterLink}
-              to="/about"
-              color="inherit"
-              underline="hover"
-            >
-              about
-            </Link>
-          </Typography>
-          <Typography
-            color="#fff"
-            variant="h5"
-            component="div"
-            sx={{ width: 100 }}
-          >
-            <Link
-              component={RouterLink}
-              to="/registration"
-              color="inherit"
-              underline="hover"
-            >
-              registration
-            </Link>
-          </Typography>
-          <Button
-            component={RouterLink}
-            to="/"
-            sx={{ color: '#ec8a59' }}
-          >
-            Main
-          </Button>
-          <Button
-            component={RouterLink}
-            to="/about"
-            sx={{ color: '#ec8a59' }}
-          >
-            About
-          </Button>
-          <Button
-            component={RouterLink}
-            to="/login"
-            sx={{ color: '#ec8a59' }}
-          >
-            Login
-          </Button>
-        </Box>
-        <IconButton
-          color="inherit"
-          sx={{ width: '50px' }}
+    <AppBar
+      position="absolute"
+      sx={{ width: '100%' }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar
+          disableGutters
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+          }}
         >
-          <ShoppingBasketOutlined />
-        </IconButton>
-      </Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+              '&:hover': {
+                color: '#e6a8d5',
+                transition: 'color 0.1s ease-in-out',
+              },
+            }}
+          >
+            <PetsIcon
+              fontSize="small"
+              sx={{ marginTop: '4px', marginRight: '5px' }}
+            />
+            PetJoy
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                >
+                  <Button
+                    component={RouterLink}
+                    to={page === 'Main' ? '/' : `/${page.toLowerCase()}`}
+                  >
+                    <Typography textAlign="center">{page}</Typography>
+                  </Button>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+              '&:hover': {
+                color: '#e6a8d5',
+                transition: 'color 0.1s ease-in-out',
+              },
+            }}
+          >
+            <PetsIcon
+              fontSize="small"
+              sx={{ marginTop: '4px', marginRight: '5px' }}
+            />
+            PetJoy
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={styleSighLinks}
+                component={RouterLink}
+                to={page === 'Main' ? '/' : `/${page.toLowerCase()}`}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'flex' }, justifyContent: 'center' }}>
+            {signLinks.map((link) => (
+              <Button
+                key={link.text}
+                onClick={handleCloseNavMenu}
+                sx={styleSighLinks}
+                component={RouterLink}
+                to={link.link}
+              >
+                {link.text}
+              </Button>
+            ))}
+
+            <Tooltip title="Open cart">
+              <IconButton
+                color="inherit"
+                sx={{ p: 0 }}
+              >
+                <Badge color="secondary">
+                  <ShoppingBasketIcon
+                    sx={{
+                      marginRight: '10px',
+                      '&:hover': {
+                        color: '#04B431',
+                        transition: 'color 0.3s ease-in-out',
+                      },
+                    }}
+                  />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Exit">
+              <IconButton color="secondary">
+                <LogoutIcon
+                  sx={{
+                    '&:hover': {
+                      color: 'red',
+                      transition: 'color 0.3s ease-in-out',
+                    },
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
-
-export default Header;
