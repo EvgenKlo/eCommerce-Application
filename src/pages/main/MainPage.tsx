9cc990086a3d7be4cb2256304e65edd82046edb0
EvgenKlo <klokovevgen@gmail.com>
2023-08-20 13:22:22 +0500

    feat: add button login on main page

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,7 +1,21 @@
 import { useAppSelector } from '@/hooks/reduxHooks';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export const MainPage: React.FC = () => {
   const customer = useAppSelector((state) => state.customers.customer);
 
-  return <h3>Hello {customer.firstName} !</h3>;
+  return (
+    <>
+      <h3>Hello {customer.firstName} !</h3>
+      <Button
+        variant="contained"
+        sx={{ margin: '2rem', '&:hover': { color: 'secondary.main' } }}
+        component={Link}
+        to={'/login'}
+      >
+        Go to Login Page
+      </Button>
+    </>
+  );
 };
