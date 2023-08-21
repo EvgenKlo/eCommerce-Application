b82499a5fce1acc50b5a58a40e1aa4d58dc90209
fasty86 <oleg.tolm1986@gmail.com>
2023-08-21 15:36:34 +0300

    feat: add show category list to catalog page

--- src/pages/catalog/CatalogPage.tsx
+++ src/pages/catalog/CatalogPage.tsx
@@ -1,3 +1,69 @@
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks';
+import { getCategories } from '@/store/slices/productSlice';
+import {
+  Button,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  Container,
+} from '@mui/material';
+
 export const CatalogPage: React.FC = () => {
-  return <div>CatalogPage</div>;
+  const categories = useAppSelector((state) => state.products.categories);
+
+  const dispatch = useAppDispatch();
+
+  const getCategoryList = (): void => {
+    dispatch(getCategories());
+  };
+
+  // useEffect(() => {
+  //   dispatch(getCategories());
+  // }, []);
+
+  return (
+    <>
+      <Container>
+        <Button
+          variant="outlined"
+          onClick={getCategoryList}
+        >
+          Load Categories list
+        </Button>
+        {/* <div>{JSON.stringify(categories)}</div> */}
+        <Box>
+          <List>
+            {categories.map(({ id, description, key, name }) => {
+              return (
+                <ListItem
+                  key={id}
+                  disablePadding
+                >
+                  <ListItemButton>
+                    <ListItemText primary={JSON.stringify(name)} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+            {/* <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Trash" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton
+              component="a"
+              href="#simple-list"
+            >
+              <ListItemText primary="Spam" />
+            </ListItemButton>
+          </ListItem> */}
+          </List>
+        </Box>
+      </Container>
+    </>
+  );
 };
