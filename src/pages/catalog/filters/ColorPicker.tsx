6c339daf00a221899251c73968db86f18a231f5d
fasty86 <oleg.tolm1986@gmail.com>
2023-08-25 16:37:17 +0300

    feat: add colorPicker component logic

--- src/pages/catalog/filters/ColorPicker.tsx
+++ src/pages/catalog/filters/ColorPicker.tsx
@@ -1,51 +1,57 @@
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks';
-import { Box, ButtonGroup } from '@mui/material';
+import { Box, ButtonGroup, Fade } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Fingerprint from '@mui/icons-material/Fingerprint';
 import Button from '@mui/material/Button';
 import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
 import { useState } from 'react';
+import { handleMouseDown } from '@/helpers/handleMouseDown';
+import { setFilterColors } from '@/store/slices/productSlice';
+
+const boxSX = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: 1,
+  justifyContent: 'space-evenly',
+  padding: 1,
+};
 
 export const ColorPicker: React.FC = () => {
   const colors = useAppSelector((state) => state.products.colors);
   const [selected, setSelected] = useState([] as string[]);
+  const dispatch = useAppDispatch();
 
   const handleClick = (color: string): void => {
     const res = selected.includes(color)
       ? [...selected].filter((item) => item !== color)
       : [...selected, color];
     setSelected(res);
+    dispatch(setFilterColors({ colors: res }));
   };
+
   return (
     <>
       <ColorLensOutlinedIcon sx={{ color: '#87a2ab' }} />
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          gap: 1,
-          justifyContent: 'space-evenly',
-          marginInline: 2,
-          // maxWidth: '150px',
-        }}
-      >
+
+      <Box sx={boxSX}>
         {colors.map((color) => {
           return (
-            // <Button
-            //   key={color}
-            //   sx={{
-            //     backgroundColor: `${color}`,
-            //     minWidth: '25px',
-            //     minHeight: '25px',
-            //     borderRadius: '50%',
-            //   }}
-            // ></Button>
             <IconButton
               key={color}
-              sx={{ border: selected.includes(color) ? `1px solid ${color}` : 'none' }}
+              sx={{
+                // border: selected.includes(color) ? `1px solid ${color}` : '1px solid transparent',
+                boxShadow: selected.includes(color) ? `0 0 15px 3px ${color}` : 'none',
+                p: 1,
+              }}
               onClick={() => handleClick(color)}
+              onMouseDown={handleMouseDown}
             >
-              <Fingerprint sx={{ color: `${color}` }} />
+              <Fade
+                in={!!color}
+                timeout={1000}
+              >
+                <Fingerprint sx={{ color: `${color}` }} />
+              </Fade>
             </IconButton>
           );
         })}
