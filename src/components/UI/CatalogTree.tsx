bd76d2fd92ccaafc13f22656cd42e029335479d0
fasty86 <oleg.tolm1986@gmail.com>
2023-08-25 16:39:27 +0300

    feat: add  All categories button with logic

--- src/components/UI/CatalogTree.tsx
+++ src/components/UI/CatalogTree.tsx
@@ -2,18 +2,30 @@ import LabelOutlinedIcon from '@mui/icons-material/LabelOutlined';
 import BeenhereOutlinedIcon from '@mui/icons-material/BeenhereOutlined';
 import { TreeView, TreeItem } from '@mui/lab';
 import { CategoryInternal } from '@/types/products';
+import { SetStateAction, SyntheticEvent } from 'react';
+
+const treeSX = {
+  Maxheight: 400,
+  maxWidth: 200,
+  textAlign: 'start',
+  m: 1,
+};
 
 export const CategoriesTree: React.FC<{
   categories: CategoryInternal[];
   handleClick: (id: string) => void;
-}> = ({ categories, handleClick }) => {
+  selected: string;
+  setSelected: (id: string) => void;
+}> = ({ categories, handleClick, selected, setSelected }) => {
   const renderTree = (cats: CategoryInternal[]) =>
     cats.map((nodes) => (
       <TreeItem
         key={nodes.id}
         nodeId={nodes.id}
         label={nodes.name.en}
-        onClick={() => handleClick(nodes.id)}
+        onClick={() => {
+          handleClick(nodes.id);
+        }}
         sx={{ borderRadius: '5%' }}
       >
         {Array.isArray(nodes.children) ? renderTree(nodes.children) : null}
@@ -26,11 +38,10 @@ export const CategoriesTree: React.FC<{
       defaultCollapseIcon={<BeenhereOutlinedIcon />}
       defaultExpanded={['root']}
       defaultExpandIcon={<LabelOutlinedIcon />}
-      sx={{
-        Maxheight: 400,
-        maxWidth: 200,
-        textAlign: 'start',
-        m: 1,
+      sx={treeSX}
+      selected={selected}
+      onNodeSelect={(event: SyntheticEvent<Element, Event>, nodeId: SetStateAction<string>) => {
+        setSelected(nodeId as string);
       }}
     >
       {renderTree(categories)}
