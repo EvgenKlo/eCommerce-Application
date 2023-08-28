da2c0599697730f095e177aa72acc35a2fca3cdb
fasty86 <oleg.tolm1986@gmail.com>
2023-08-28 10:32:53 +0300

    refactor: optimize imports

--- src/pages/catalog/filters/ColorPicker.tsx
+++ src/pages/catalog/filters/ColorPicker.tsx
@@ -1,8 +1,6 @@
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks';
-import { Box, Fade } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import Fingerprint from '@mui/icons-material/Fingerprint';
-import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
+import { Box, Fade, IconButton } from '@mui/material';
+import { Fingerprint, ColorLensOutlined as ColorLensOutlinedIcon } from '@mui/icons-material';
 import { useEffect, useState } from 'react';
 import { handleMouseDown } from '@/helpers/handleMouseDown';
 import { setFilterColors, getProductsWithFilter } from '@/store/slices/productSlice';
@@ -25,24 +23,27 @@ export const ColorPicker: React.FC = () => {
     const res = selected.includes(color)
       ? [...selected].filter((item) => item !== color)
       : [...selected, color];
+
     setSelected(res);
+
     dispatch(setFilterColors({ colors: res }));
   };
+
   useEffect(() => {
     if (!filterColors?.length) setSelected([]);
+
     dispatch(getProductsWithFilter());
   }, [filterColors]);
+
   return (
     <>
       <ColorLensOutlinedIcon sx={{ color: '#87a2ab' }} />
-
       <Box sx={boxSX}>
         {colors.map((color) => {
           return (
             <IconButton
               key={color}
               sx={{
-                // border: selected.includes(color) ? `1px solid ${color}` : '1px solid transparent',
                 boxShadow: selected.includes(color) ? `0 0 15px 3px ${color}` : 'none',
                 p: 1,
               }}
