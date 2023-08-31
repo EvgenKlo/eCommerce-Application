82fa56629826778791f3d8760ff8f5e18b266e8a
fasty86 <oleg.tolm1986@gmail.com>
2023-08-31 21:33:23 +0300

    fix: fix  parsing token data from ls

--- src/hooks/AuthHooks.ts
+++ src/hooks/AuthHooks.ts
@@ -3,9 +3,9 @@ import { getApiRoot } from '@/api/lib/Client';
 import { API } from '@/api/API';
 import { useAppDispatch } from '@/hooks/reduxHooks';
 import { setAuthorization, setApi, SignInByToken } from '@/store/slices/customerSlice';
+import { type TokenStore } from '@commercetools/sdk-client-v2';
 
 export const useAuth = () => {
-  const [token, setToken] = useState('');
   const [auth, setAuth] = useState(false);
   const [apiClient, setApiClient] = useState(getApiRoot('anonimous'));
   const [apiInstance, setApiInstance] = useState(new API(apiClient));
@@ -17,13 +17,12 @@ export const useAuth = () => {
 
   useEffect(() => {
     if (localStorage.getItem('tokendata')) {
-      const tokenLS = localStorage.getItem('tokendata') as string;
-      setToken(tokenLS || ('' as string));
-      setApiClient(getApiRoot('token', { token: token }));
+      const tokenLS = JSON.parse(localStorage.getItem('tokendata')!) as TokenStore;
+      setApiClient(getApiRoot('token', { token: tokenLS.refreshToken }));
       setApiInstance(new API(apiClient));
       void dispatch(setAuthorization(true));
       void dispatch(setApi(apiInstance));
-      void dispatch(SignInByToken(tokenLS));
+      void dispatch(SignInByToken(tokenLS.refreshToken!));
     }
   }, [auth]);
   return [changeAuth];
