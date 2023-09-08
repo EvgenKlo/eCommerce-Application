acd0bb45f97d704b522ac536aa7f7f085e4802ec
EvgenKlo <118301743+EvgenKlo@users.noreply.github.com>
2023-09-08 20:56:37 +0500

    fix: bug with enum ClientType

--- src/types/Enums.ts
+++ src/types/Enums.ts
@@ -4,3 +4,10 @@ export enum SortOptions {
   color = 'variants.attributes.color.en',
   size = 'variants.attributes.size.en',
 }
+
+export enum ClientType {
+  regular = 'regular',
+  anonymous = 'anonymous',
+  password = 'password',
+  token = 'token',
+}
--- src/types/apiClient.d.ts
+++ src/types/apiClient.d.ts
@@ -1,7 +1,1 @@
-export enum clientType {
-  regular = 'regular',
-  anonymous = 'anonymous',
-  password = 'password',
-  token = 'token',
-}
 export type returnType<T> = Promise<{ data: T | undefined; error: string }>;
